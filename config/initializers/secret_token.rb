9d63d74611e5f12a07ecc9b2ac2d1d028d449bca
Russ Whitney <russ@sawtoothideas.com>
2011-10-27 08:27:37 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Highbridge::Application.config.secret_token = '650c889abc2e88aac48b5be04e47b51ef8cb511f5e9859578af74db2dc25200756f8587d7fe5d32eca3173c767e8a6bf7f175aa06db253dfe9d3fd4600988e11'
