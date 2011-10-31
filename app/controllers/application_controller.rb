cde8e3b261abbaab4909e5a9784fae5756cbf7fc
Russ Whitney <russ@sawtoothideas.com>
2011-10-31 17:24:04 -0600

    fixed calendar shift display

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,4 @@ class ApplicationController < ActionController::Base
   # this changes it so only the helper with the same name is available
   clear_helpers
 
-protected
-  def old_authenticate
-    authenticate_or_request_with_http_basic do |username, password|
-      if @user
-        @user
-      else
-        @user = Member.validate_login username, password
-      end
-    end
-  end
 end
