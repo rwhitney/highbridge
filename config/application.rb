293b8a11349b294a788dc53a9d16e9780ac98a0a
Russ Whitney <russ@sawtoothideas.com>
2011-11-10 16:28:23 -0700

    added list of precompiled assets

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module Highbridge
         
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    config.assets.precompile += ['shiftedit2.css','members_only.css','calendar.css','welcome.css',
+      'calendar.js']
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
