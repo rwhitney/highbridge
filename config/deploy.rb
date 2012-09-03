c3f2a8c111e659bd6724c13569624ef42d4ba717
rwhitney <russ@whitneyhome.com>
2012-09-03 09:46:44 -0600

    changed default ruby version to 1.9.3-p194

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ default_run_options[:pty] = true
 
 $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
-set :rvm_ruby_string, '1.9.2'        # Or whatever env you want it to run in.
+set :rvm_ruby_string, '1.9.3-p194'        # Or whatever env you want it to run in.
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
